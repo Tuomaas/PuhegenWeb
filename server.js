0a338025a451714f6398e0b2242cd4fb360c5f24
Tuomas Toivonen <tuomas.a.toivonen@gmail.com>
2018-08-18 21:13:47 +0300

    asd

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ let luoPuhe = (pituus) =>{
 //"build": "cd puhegen && build",  
 //Port jolle ohjelma avataan
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
