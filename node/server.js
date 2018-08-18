bd1d036c5d8846deb3727684116a7d385ac846b5
Tuomas Toivonen <tuomas.a.toivonen@gmail.com>
2018-08-18 10:49:22 +0300

    Innit commit

---
+++ node/server.js
@@ -0,0 +1,97 @@
+
+
+//C:\Users\Tuomas\Desktop\mooc\Puhegen\Java\javaClasses\Server.class
+//'java',['-classpath' ,'C:\\Users\\Tuomas\\Desktop\\mooc\\Puhegen\\Java\\javaClasses', 'Server']
+
+//vaadittavat luokat
+const { spawn } = require('child_process');
+const iconv = require('iconv-lite');
+const express = require('express')
+const cors = require('cors')
+
+
+
+//app 
+const app = express();
+let router = express.Router()
+let currentPuhe = "TestiALOITUS"; //Edellisin palautettu puhe
+app.use(cors())
+
+//Käynnistetään puhegen JAVA ohjelma 
+const child = require('child_process').exec(
+    'java -classpath C:\\Users\\Tuomas\\Desktop\\mooc\\Puhegen\\Java\\javaClasses Server',
+    //encoding --> buffer,joka aihettaa sen että callbackit palauttaa kirjaimet muodossa esim(123,332,342,234)
+    //muuten ä å ö tuhoutuisivat. Jostain syystä utf8 ei tue niitä?
+    { encoding: 'buffer' },
+    function(err, stdout, stderr) {
+      console.log("Stdout function :" ,stdout)
+   }
+);
+
+//Callback 
+child.stdout.on('data', function(data) {
+
+  //Tähän tulee data ohjelmasta se on buffer muodossa joten se pitää muuntaa oikeaan muotoon
+  outData = iconv.decode(Buffer.from(data),'ISO-8859-4');
+  console.log(outData);
+
+  //asetetaan saatu puhe(outData) nykyiseksi puheeksi
+  currentPuhe = outData;
+
+});
+child.stderr.on('data', function(data) {
+  console.log('std error: ' + data);
+  console.log("type of data : ",typeof data)
+});
+child.on('close', function(code) {
+  console.log('closing code: ' + code);
+})
+
+
+app.use('/puhe',function (req, res, next) {
+  //child.stdin.write("18 \n") //TODO luo puhe vain /luoUusiPuhe/:length käyttäen muuten voidaan palauttaa edellinen uutena
+  console.log('avataan /puhe:')
+  console.log('Time:', Date.now())
+  next()
+})
+
+app.get('/luoUusiPuhe/:length',(request, response) =>{
+
+ 
+  //Luodaan pyynnöstä java ohjelmalle kirjoitettava pyyntö muotoa --> pituus(int) \n(käyttäytyy kuten ENTER)
+  const length = request.params.length;
+  console.log("Vastaan otettiin uuden lauseen teon pyyntö. Pituus : " , length)
+  luoPuhe(length)
+  
+  //TODO miten palauttaa vasta kun puhe on valmis?
+  response.end("Pyyntö vastaan otettu");
+})
+
+
+app.get('/',(request, response) =>{
+
+  response.send("Moi")
+  
+})
+
+app.get('/puhe',(request, response) =>{
+
+  response.send(currentPuhe);
+  
+})
+
+let luoPuhe = (pituus) =>{
+
+  pyyntö = pituus + "\n"
+  child.stdin.write(pyyntö)
+}
+
+
+
+
+//Port jolle ohjelma avataan
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ puhegen/src/App.js
@@ -0,0 +1,168 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios' //http pyyntöjen teko kirjasto 
+
+
+
+
+class Title extends Component{
+  render(){
+    return(
+      <div className="Title">
+        <h2>Tervetuloa vaalipuhe/pöhinä generaattoriin!</h2>
+      </div>
+    )
+  }
+}
+
+class Info extends Component{
+  render(){
+    return(
+      <div className="Info">
+        <h2>Info</h2>
+        <p>Tämän ohjelma luo "satunnaisesti" tekstiä ylen vaalikoneen kunnallisvaalien ehdokkaiden vastauksista. </p>
+        <div><span>Datan lähde : </span><a href="https://yle.fi/uutiset/3-9526290">Yle</a></div>
+        <br/>
+        <p>Tällä hetkellä ohejelma on toteutettu käyttäen (Node.js,React.js ja Java). Palvelin ei ole kovin kehittynyt ja periaatteessa kykenee vain palvelemaan yhtä henkiöä kerralla.</p>
+        <p>Copyright Tuomas</p>
+      </div>
+    )
+  }
+}
+
+class Lause extends Component{
+  render(){
+
+
+    if(this.props.ladataanko === true){
+      return(
+        <div className="Lause">
+          <div className="Lataus"></div>
+        </div>
+      )
+    }
+
+    if(this.props.puhe === ""){
+      return(
+        <div className="Lause">
+          <p>aseta lauseen pituus ja paina nappia!</p>
+        </div>
+      )
+    }
+
+
+    return(
+      <div className="Lause">
+        <p>{this.props.puhe}</p>
+        </div>
+    )
+  }
+}
+
+class PuhegenAsetukset extends Component{
+
+  render(){
+    return(
+      <div className = "InputForm">
+        <form onSubmit = {this.props.createNewPuheReq}>
+
+        <div>
+          Luotavan lauseen sana määrä : <input 
+          value = {this.props.puhePituus}
+          onChange = {this.props.handlePituusChange}
+          />
+        </div>
+
+        <div>
+          <button className="Button" type="submit">Luo vaalipöhinää</button>
+        </div>
+
+      </form>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      puhe : "",
+      puhePituus : 0,
+      ladataanko : false
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <Title />
+          <PuhegenAsetukset  handlePituusChange={this.handlePituusChange} createNewPuheReq={this.createNewPuheReq}/>
+          <Lause puhe={this.state.puhe} ladataanko={this.state.ladataanko}/>
+          <Info />
+      </div>
+    );
+  }
+
+
+  handlePituusChange = (event) =>{
+    this.setState({puhePituus : event.target.value})
+  }
+
+  testIfReqIsFresh = (response,serverReqPuhe) =>{
+     //Jos palvelimen vastaus on sama kun aikaisemmin --> kysytään uudestaan joka 2,5 sec TODO onko viisasta
+     console.log("Katsotaan onko saatu vastaus uusi. Vastaus: ", response.data , " === nykyinen: " , this.state.puhe )
+     if(response.data === this.state.puhe){
+      //Huomattiin, että saatu vastaus on sama kuin mikä meillä on jo joten pyydetään uudestaan sekunnin päästä
+      return new Promise( () =>{
+        setTimeout( () =>{
+          axios.get(serverReqPuhe).then(response =>{
+            this.testIfReqIsFresh(response,serverReqPuhe)
+          })
+        },2500)
+      })
+
+    }else{
+      this.setState({puhe : response.data,ladataanko:false}, () => console.log("Muokataan state.puhe uusi state : ", this.state.puhe))
+    }
+  }
+
+  createNewPuheReq = (event) =>{
+    event.preventDefault()
+
+    console.log("Kysytään palvelimelta uutta lausetta pituudelta :",this.state.puhePituus)
+
+    let serverReqCreate = 'http://localhost:3001/luoUusiPuhe/' + this.state.puhePituus
+    let serverReqPuhe = 'http://localhost:3001/puhe/'
+
+    //Kutsutaan palvelin 'http://localhost:3001/luoUusiPuhe/ osoitteella ja sen jälkeen kun saadaan vastaus suoritetaan alempi then()
+    axios.get(serverReqCreate).then(renponse =>{
+      //Luodaan uusi promise jota käytetään aync timeouttina eli odotetaan 2sec, ja sen jälkeen kysytään palvelimelta
+      //uutta lausetta.
+      this.setState({ladataanko:true})
+      return new Promise( () =>{
+        setTimeout( () =>{
+          //tehdään pyyntö palvelimelle ja tuloksen saannin jälkeen asetetaan se stateen.
+          axios.get(serverReqPuhe).then(response =>{
+            
+           this.testIfReqIsFresh(response,serverReqPuhe)
+            //console.log("vastaus : ", response.data)
+            
+          })
+
+          
+        },2000)
+
+      })
+
+    }).catch( error =>{
+      //Virhe on tapahtunut
+      console.log("Promise virhe : ", error)
+  })
+
+
+  }
+
+}
+
+export default App;
